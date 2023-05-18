6b5aa01a85bd2e3f2f5a62b99d3ca023a0ee5792
thetamimahmed <wwwtamim000@gmail.com>
2023-05-18 22:23:40 +0600

    added banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,8 +1,13 @@
 
 const Banner = () => {
     return (
-        <div className="bg-lightPink h-screen mb-5 bg-cover bg-center bg-[url('https://i.ibb.co/RB8K07J/pexels-mike-bird-381228.jpg')]">
-            <h1>banner</h1>
+        <div className="bg-lightPink mb-5 h-screen bg-cover bg-center bg-[url('https://i.ibb.co/RB8K07J/pexels-mike-bird-381228.jpg')]">
+            <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-r from-[#151515ca] to-[rgba(21,21,21,0.66)]">
+                <h1 className="text-blue text-5xl font-bold text-center my-2 w-3/4 leading-tight">Rev Up Your Imagination! Discover the Ultimate Toy Car Wonderland!</h1>
+                <p className="text-lightPink text-xl  text-center my-2 w-3/4">
+                    Unleash your imagination with our thrilling toy car collection. From sleek sports cars to rugged off-road vehicles, ignite endless fun and excitement. Explore our selection now!</p>
+                <button className="primary-btn">Explore Now</button>
+            </div>
         </div>
     );
 };
