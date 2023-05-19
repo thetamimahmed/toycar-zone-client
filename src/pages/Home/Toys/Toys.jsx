0231eadba58539df644da3dd7b8872ad6f4ef81a
thetamimahmed <wwwtamim000@gmail.com>
2023-05-20 02:42:10 +0600

    added toy category section by switching tab

--- src/pages/Home/Toys/Toys.jsx
+++ src/pages/Home/Toys/Toys.jsx
@@ -1,30 +1,94 @@
 // import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const Toys = () => {
-    // const [toys, setToys] = useState([])
-    const tab1 = () =>{
-        console.log("clicked 1")
-        
-    }
+    const [category, setCategory] = useState("SportsCar")
+    const [toys, setToys] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/alltoy/${category}`)
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [category])
+
     return (
-        <div className="max-w-6xl mx-auto mb-10">
+        <div className="max-w-6xl mx-auto my-20">
             <Tabs>
                 <TabList className="flex justify-center space-x-10">
-                    <Tab onClick={tab1} className="tab-btn">Sports Car</Tab>
-                    <Tab onClick={tab1} className="tab-btn">Mini Truck</Tab>
-                    <Tab onClick={tab1} className="tab-btn">Police Car</Tab>
+                    <Tab onClick={() => setCategory("SportsCar")} className="tab-btn">Sports Car</Tab>
+                    <Tab onClick={() => setCategory("Truck")} className="tab-btn">Mini Truck</Tab>
+                    <Tab onClick={() => setCategory("PoliceCar")} className="tab-btn">Police Car</Tab>
                 </TabList>
 
-                <TabPanel>
-                    <h2>Any content 1</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Any content 2</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Any content 3</h2>
-                </TabPanel>
+                <div className="my-14">
+                    <TabPanel className="grid grid-cols-2 place-items-center">
+                        {
+                            toys.map(toy => <div key={toy._id} className="card card-compact w-96 bg-base-100 shadow-xl">
+                                <figure><img className="h-[300px] rounded-lg" src={toy?.image} alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{toy?.name}</h2>
+                                    <p className="font-bold">Price: ${toy?.price}</p>
+                                    <div>
+                                        <Rating
+                                            style={{ maxWidth: 100 }}
+                                            value={toy?.rating}
+                                            readOnly
+                                        />
+                                    </div>
+                                    <div className="card-actions justify-center">
+                                        <button className="toyDetails-btn"><Link to={`/toys/${toy._id}`}>View Details</Link></button>
+                                    </div>
+                                </div>
+                            </div>)
+                        }
+                    </TabPanel>
+                    <TabPanel className="grid grid-cols-2 place-items-center">
+                        {
+                            toys.map(toy => <div key={toy._id} className="card card-compact w-96 bg-base-100 shadow-xl">
+                                <figure><img className="h-[300px] rounded-lg" src={toy?.image} alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{toy?.name}</h2>
+                                    <p className="font-bold">Price: ${toy?.price}</p>
+                                    <div>
+                                        <Rating
+                                            style={{ maxWidth: 100 }}
+                                            value={toy?.rating}
+                                            readOnly
+                                        />
+                                    </div>
+                                    <div className="card-actions justify-center">
+                                        <button className="toyDetails-btn"><Link to={`/toys/${toy._id}`}>View Details</Link></button>
+                                    </div>
+                                </div>
+                            </div>)
+                        }
+                    </TabPanel>
+                    <TabPanel className="grid grid-cols-2 place-items-center">
+                        {
+                            toys.map(toy => <div key={toy._id} className="card card-compact w-96 bg-base-100 shadow-xl">
+                                <figure><img className="h-[300px] rounded-lg" src={toy?.image} alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{toy?.name}</h2>
+                                    <p className="font-bold">Price: ${toy?.price}</p>
+                                    <div>
+                                        <Rating
+                                            style={{ maxWidth: 100 }}
+                                            value={toy?.rating}
+                                            readOnly
+                                        />
+                                    </div>
+                                    <div className="card-actions justify-center">
+                                        <button className="toyDetails-btn"><Link to={`/toys/${toy._id}`}>View Details</Link></button>
+                                    </div>
+                                </div>
+                            </div>)
+                        }
+                    </TabPanel>
+                </div>
             </Tabs>
         </div>
     );
