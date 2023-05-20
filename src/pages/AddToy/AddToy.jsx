712303b0e92432fef04e53457e506f4121b45858
thetamimahmed <wwwtamim000@gmail.com>
2023-05-20 15:58:19 +0600

    implement search functionality

--- src/pages/AddToy/AddToy.jsx
+++ src/pages/AddToy/AddToy.jsx
@@ -6,112 +6,113 @@ import "react-toastify/dist/ReactToastify.css";
 const AddToy = () => {
     const { user } = useContext(AuthContext)
 
-        const handleAddToy = event => {
-            event.preventDefault()
-            const form = event.target;
-            const name = form.toyName.value;
-            const seller = form.sellerName.value;
-            const email = user?.email;
-            const category = form.category.value;
-            const price = form.price.value;
-            const rating = form.rating.value;
-            const quantity = form.quantity.value;
-            const image = form.photo.value;
-            const description = form.description.value;
-            const newToy = {
-                name, seller, email, category, price, rating, quantity, image, description
-            }
-            console.log(newToy)
-
-            fetch('http://localhost:5000/toys', {
-                method: 'POST',
-                headers: { 'content-type': 'application/json' },
-                body: JSON.stringify(newToy)
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    if (data.insertedId) {
-                        toast("Toy Added Successfully");                       
-                    }
-                })
+    const handleAddToy = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.toyName.value;
+        const seller = form.sellerName.value;
+        const email = user?.email;
+        const category = form.category.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const quantity = form.quantity.value;
+        const image = form.photo.value;
+        const description = form.description.value;
+        const newToy = {
+            name, seller, email, category, price, rating, quantity, image, description
         }
-        return (
-            <div>
-                <h1 className='text-4xl font-bold text-pink text-center mt-5'>Add Your Toy</h1>
-                <form onSubmit={handleAddToy}>
-                    <div className="p-5 mb-10">
-                        <div className="card-body grid grid-cols-1 lg:grid-cols-2">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text" >Toy Name</span>
-                                </label>
-                                <input type="name" name="toyName" placeholder="toy name" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text" >Seller Name</span>
-                                </label>
-                                <input type="name" name="sellerName" defaultValue={user?.displaName} placeholder="name" required className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text" >Seller Email</span>
-                                </label>
-                                <input type="email" name="email" defaultValue={user?.email} placeholder="email" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text" >Category</span>
-                                </label>
-                                <div className="input-group">
-                                    <select required name="category" className="select select-bordered">
-                                        {/* <option disabled selected>Pick category</option> */}
-                                        <option value={"SportsCar"}>SportsCar</option>
-                                        <option value={"Truck"}>Truck</option>
-                                        <option value={"PoliceCar"}>PoliceCar</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Price</span>
-                                </label>
-                                <input type="number" name="price" placeholder="price" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Rating</span>
-                                </label>
-                                <input type="number" name="rating" placeholder="Rating" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Quantity</span>
-                                </label>
-                                <input type="number" name="quantity" placeholder="quantity" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Photo URL</span>
-                                </label>
-                                <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Description</span>
-                                </label>
-                                <textarea required placeholder="Description" name="description" className="textarea textarea-bordered textarea-lg w-full" ></textarea>
+        console.log(newToy)
+
+        fetch('http://localhost:5000/toys', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(newToy)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.insertedId) {
+                    toast("Toy Added Successfully");
+                }
+            })
+    }
+    return (
+        <div>
+            <h1 className='text-4xl font-bold text-pink text-center mt-5'>Add Your Toy</h1>
+            
+            <form onSubmit={handleAddToy}>
+                <div className="p-5 mb-10">
+                    <div className="card-body grid grid-cols-1 lg:grid-cols-2">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text" >Toy Name</span>
+                            </label>
+                            <input type="name" name="toyName" placeholder="toy name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text" >Seller Name</span>
+                            </label>
+                            <input type="name" name="sellerName" defaultValue={user?.displaName} placeholder="name" required className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text" >Seller Email</span>
+                            </label>
+                            <input type="email" name="email" defaultValue={user?.email} placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text" >Category</span>
+                            </label>
+                            <div className="input-group">
+                                <select required name="category" className="select select-bordered w-full max-w-xs">
+                                    {/* <option disabled selected>Pick category</option> */}
+                                    <option value={"SportsCar"}>SportsCar</option>
+                                    <option value={"Truck"}>Truck</option>
+                                    <option value={"PoliceCar"}>PoliceCar</option>
+                                </select>
                             </div>
                         </div>
-                        <div className="form-control mt-6 w-3/4 mx-auto">
-                            <input className="btn primary-btn" type="submit" value="ADD TOY" />
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Price</span>
+                            </label>
+                            <input type="number" name="price" placeholder="price" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Rating</span>
+                            </label>
+                            <input type="number" name="rating" placeholder="Rating" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Quantity</span>
+                            </label>
+                            <input type="number" name="quantity" placeholder="quantity" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Description</span>
+                            </label>
+                            <textarea required placeholder="Description" name="description" className="textarea textarea-bordered textarea-lg w-full" ></textarea>
                         </div>
                     </div>
-                </form>
-                <ToastContainer />
-            </div>
-        );
-    };
+                    <div className="form-control mt-6 w-3/4 mx-auto">
+                        <input className="btn primary-btn" type="submit" value="ADD TOY" />
+                    </div>
+                </div>
+            </form>
+            <ToastContainer />
+        </div>
+    );
+};
 
-    export default AddToy;
+export default AddToy;
--- src/pages/AllToy/AllToy.jsx
+++ src/pages/AllToy/AllToy.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { useLoaderData, Link } from "react-router-dom"
 const AllToy = () => {
     const allToys = useLoaderData()
-    console.log(allToys)
+    const [toys, setToys] = useState(allToys)
+    const [searchText, setSearchText] = useState("")
+    const handleSearchToy = () => {
+        fetch(`http://localhost:5000/getToysBySearch/${searchText}`)
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }
     return (
         <div className="overflow-x-auto mb-10 p-5">
+            {/* search by toy name  */}
+            <div className="my-5 flex justify-center">
+                <input onChange={(e) => { setSearchText(e.target.value) }} type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
+                <button onClick={handleSearchToy} className="search-btn rounded-none">Search</button>
+            </div>
             <table className="table w-full">
                 {/* head */}
                 <thead>
@@ -18,18 +30,18 @@ const AllToy = () => {
                 </thead>
                 <tbody>
                     {
-                        allToys.map((toy, index)=> {
-                        return <tr key={toy._id}>
-                            <th>{index+1}</th>
-                            <td>{toy.seller}</td>
-                            <td>{toy.name}</td>
-                            <td>{toy.category}</td>
-                            <td>{toy.price}</td>
-                            <td><button className="toyDetails-btn"><Link to={`/toys/${toy._id}`}>View Details</Link></button></td>
-                        </tr>
+                        toys.map((toy, index) => {
+                            return <tr key={toy._id}>
+                                <th>{index + 1}</th>
+                                <td>{toy.seller}</td>
+                                <td>{toy.name}</td>
+                                <td>{toy.category}</td>
+                                <td>{toy.price}</td>
+                                <td><button className="toyDetails-btn"><Link to={`/toys/${toy._id}`}>View Details</Link></button></td>
+                            </tr>
                         })
                     }
-                    
+
                 </tbody>
             </table>
         </div>
